6192993baad300a578631b4246af0bd7c6c118c0
DaniielZ <34136852+DaniielZ@users.noreply.github.com>
2024-12-01 23:14:38 +0200

    added forward kinematic solver

--- hexpod_manipulator/forward_kinematics.py
+++ hexpod_manipulator/forward_kinematics.py
@@ -4,17 +4,25 @@ import numpy as np
 import math
 from scipy.spatial.transform import Rotation as R
 from scipy.optimize import fsolve
+import hexpod
 
 
 
 class ForwardKinematics:
 
-  def PadPosRotSolverEquation(pad_trans_rot, pad_leg_connection, base_leg_connection, base_translation, base_rotation, leg_length):
-    pad_translation = pad_trans_rot[0:3]
-    pad_rotation = pad_trans_rot[3:6]
+  def PadPosRotSolverEquation(self, inputs, pad_leg_connection, base_leg_connection, base_translation, base_rotation, leg_length):
+    pad_translation = inputs[0:3]
+    pad_rotation = inputs[3:6]
     results = np.zeros(6)
-    pad_leg_connection_3d_pos = pad_leg_connection*R.from_euler(pad_rotation) + pad_translation
-    base_leg_connection_3d_pos = base_leg_connection*R.from_euler(base_rotation) + base_translation
+    if (pad_rotation[0] > np.pi/2) or (pad_rotation[0] < -np.pi/2):
+      return np.ones(6)*np.inf
+    if (pad_rotation[1] > np.pi/2) or (pad_rotation[1] < -np.pi/2):
+      return np.ones(6)*np.inf
+    
+    # calculate the 3d pos of the base and pad leg connections
+    base_leg_connection_3d_pos = hexpod.Hexpod.PlatformLegConnectionPos3D(base_leg_connection, base_translation, base_rotation)
+    pad_leg_connection_3d_pos = hexpod.Hexpod.PlatformLegConnectionPos3D(pad_leg_connection, pad_translation, pad_rotation)
+    
     for i in range(6):
       results[i] = np.linalg.norm(pad_leg_connection_3d_pos[i] - base_leg_connection_3d_pos[i]) - leg_length[i]
     return results
@@ -22,17 +30,24 @@ class ForwardKinematics:
 
   def PadPosRotSolver(self, Hexpod, max_error = 0.1):
     # solve the position and rotation of the pad and the base
+    #limit pad rotation
     
-    result = fsolve(self.PadPosRotSolverEquation, np.zeros(6), args=(Hexpod.pad_leg_connections, Hexpod.base_leg_connections, Hexpod.base_translation, Hexpod.base_rotation, Hexpod.leg_length), full_output=True)
-    max_deviation = np.max(np.abs(result.infodict['fvec']))
+    solution, infodict, ier, msg = fsolve(self.PadPosRotSolverEquation, np.zeros(6), 
+                                          args=(Hexpod.pad_leg_connections, 
+                                                Hexpod.base_leg_connections, 
+                                                Hexpod.base_translation, 
+                                                Hexpod.base_rotation, 
+                                                Hexpod.leg_length), full_output=True)
+    
+    max_deviation = np.max(np.abs(infodict['fvec']))
     
     if max_deviation > max_error:
-      print("Error: Could not solve for pad position and rotation")
-      print("Max deviation: ", max_deviation)
-      print("Result: ", result[0:6])
-      return False
+        print("Error: Could not solve for pad position and rotation")
+        print("Max deviation: ", max_deviation)
+        print("Result: ", solution)
+        return False
     else:
-      Hexpod.pad_translation = result[0:3]
-      Hexpod.pad_rotation = result[3:6]
-      return True
+        Hexpod.pad_translation = solution[0:3]
+        Hexpod.pad_rotation = solution[3:6]
+        return True
 
--- hexpod_manipulator/hexpod.py
+++ hexpod_manipulator/hexpod.py
@@ -1,14 +1,15 @@
 import numpy as np
-import forward_kinematics
+import matplotlib.pyplot as plt
+from scipy.spatial.transform import Rotation as R
 
 #3d class for a hex pod 
 class Hexpod:
   base_translation = np.zeros(3)
-  base_rotation = np.zeros((3,3))
+  base_rotation = np.zeros(3) #euler angles in radians
   base_leg_connections = np.zeros((6,2))
   
   pad_translation = np.zeros(3)
-  pad_rotation = np.zeros((3,3))
+  pad_rotation = np.zeros(3) #euler angles in radians
   pad_leg_connections = np.zeros((6,2)) 
   
   # 3d array of the 6 legs 
@@ -26,26 +27,79 @@ class Hexpod:
     self.leg_length = leg_min_length*np.ones(6)
     self.leg_min_length = leg_min_length
     self.leg_max_length = leg_max_length
-    
-    # solve the position and ortof the pad and the base
-    forward_kinematics.ForwardKinematics.PadPosRotSolver(self, self.leg_max_length*0.01)
-    
-    
+    return
   
     # everything except the pad postion and rotation is known
   def CalculateHexagon(self, radius, short_vs_long_angle_ratio): 
     hexagon = np.zeros((6,2))
     angles = np.zeros(6)
-    short_edge_deg = np.deg2rad(360/6)*short_vs_long_angle_ratio
-    long_edge_deg = np.deg2rad(360/6) - short_edge_deg
+    short_edge_deg = (360/3)*short_vs_long_angle_ratio
+    long_edge_deg = (360/3) - short_edge_deg
     
     for i in range(6):
       if i%2 == 0:
           angles[i] = angles[i-1]+short_edge_deg
       else:
           angles[i] = angles[i-1]+long_edge_deg     
-      hexagon[i] = [radius*np.cos(angles[i]), radius*np.sin(angles[i])]
+      hexagon[i] = [radius*np.cos(np.deg2rad(angles[i])), radius*np.sin(np.deg2rad(angles[i]))]
     
     return hexagon
+  
+  def PrintStatus(self):
+    print("Base leg connections: ", self.base_leg_connections)
+    print("Pad leg connections: ", self.pad_leg_connections)
+    print("Leg length: ", self.leg_length)
+    print("Pad translation: ", self.pad_translation)
+    print("Pad rotation: ", self.pad_rotation)
+    print("Base translation: ", self.base_translation)
+    print("Base rotation: ", self.base_rotation)
+    return
+  
+  
+  # input is trannslation and rotation of the pad rad ians
+  def PlatformLegConnectionPos3D(leg_connections_2d, translation, rotation):
+    # Apply rotations
+    rotation_matrix = R.from_euler('xyz', rotation).as_matrix()
+    # Make a 3D array of the 2D leg connections
+    leg_connections_3d = np.hstack((leg_connections_2d, np.zeros((6,1))))
+    # Apply translations and rotations
+    
+    for i in range(6):
+      leg_connections_3d[i] = (leg_connections_3d[i] @ rotation_matrix.T) + translation
+    return leg_connections_3d
+    
+  def VisualizeAllLegConnectionPositions(self):
+    # Apply rotations
 
+    # Apply translations and rotations
+    pad_leg_pos = Hexpod.PlatformLegConnectionPos3D(self.pad_leg_connections, self.pad_translation, self.pad_rotation)
+    base_leg_pos = Hexpod.PlatformLegConnectionPos3D(self.base_leg_connections, self.base_translation, self.base_rotation)
+    
+    # 3D plot
+    fig = plt.figure()
+    ax = fig.add_subplot(111, projection='3d')
+    ax.scatter(pad_leg_pos[:, 0], pad_leg_pos[:, 1], pad_leg_pos[:, 2], c='r', marker='o')
+    ax.scatter(base_leg_pos[:, 0], base_leg_pos[:, 1], base_leg_pos[:, 2], c='b', marker='o')
+
+    # Connect the legs
+    for i in range(6):
+        ax.plot([pad_leg_pos[i, 0], base_leg_pos[i, 0]], 
+                [pad_leg_pos[i, 1], base_leg_pos[i, 1]], 
+                [pad_leg_pos[i, 2], base_leg_pos[i, 2]], c='k')
+    
+    plt.show()
+    return
+  def VisualizeHexagon(self, hexagon):
+    fig, ax = plt.subplots()
+    ax.scatter(hexagon[:,0], hexagon[:,1])
+    #label
+    for i in range(6):
+        ax.annotate(str(i), (hexagon[i,0], hexagon[i,1]))
+    plt.show()
+    return
+# 1 is max, 0 is min
+  def SetLegLengthFromRatio(self, ratio_array):
+    ratio_array = np.array(ratio_array)  # Convert to NumPy array
+    self.leg_length = self.leg_min_length + ratio_array * (self.leg_max_length - self.leg_min_length)
+    return
   
